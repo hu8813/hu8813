901d95fa9545ad4e9312a09cfd70d87508a42b62
huaydin <38990435+hu8813@users.noreply.github.com>
2023-09-22 08:28:28 +0200

    Update install_francinette.sh

--- install_francinette.sh
+++ install_francinette.sh
@@ -7,3 +7,5 @@ echo 'alias francinette="bash ~/francinette/tester.sh"' >> ~/.zshrc
 sed -i 's/python /python3 /' ~/francinette/tester.sh
 source ~/.bashrc
 source ~/.zshrc
+echo "Installation complete."
+echo "You can now use 'francinette' by typing 'francinette' in a new terminal and folder where your project is."
